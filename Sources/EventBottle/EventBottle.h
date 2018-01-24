d8a134830641ed1f71a426711609eb26b8c7bda6
slightair <arksutite@gmail.com>
2018-01-24 18:47:25 +0900

    Support Carthage

---
+++ Sources/EventBottle/EventBottle.h
@@ -0,0 +1,19 @@
+//
+//  EventBottle.h
+//  EventBottle
+//
+//  Created by tomohiro-moro on 2018/01/24.
+//  Copyright © 2018 slightair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventBottle.
+FOUNDATION_EXPORT double EventBottleVersionNumber;
+
+//! Project version string for EventBottle.
+FOUNDATION_EXPORT const unsigned char EventBottleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventBottle/PublicHeader.h>
+
+
