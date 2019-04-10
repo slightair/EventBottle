ab6a53fd9f5af3d52fb6f10b6f2bf204366ef0bc
slightair <arksutite@gmail.com>
2019-04-10 18:23:35 +0900

    Use Swift5

--- Sources/EventBottle/EventViewer/EventCell.swift
+++ Sources/EventBottle/EventViewer/EventCell.swift
@@ -31,7 +31,7 @@ class EventCell: UITableViewCell {
         }
     }
 
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         setUpSubviews()
