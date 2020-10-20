8580233595fa96b4b19cff32903686017b8b0d7a
Yuya Hirayama <yuya-hirayama@cookpad.jp>
2020-10-21 08:03:26 +0900

    Fix version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "EventBottle",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v10)],
     products: [
         .library(
             name: "EventBottle",
